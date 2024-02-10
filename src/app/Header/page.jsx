7367102cf08119d408fa3ceff5058ea1332549c1
Muhammad Huzaifa <mhuzaifa134@gmail.com>
2024-02-11 03:02:00 +0500

    code

--- src/app/Header/page.jsx
+++ src/app/Header/page.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Image from "next/image";
 import blue_logo from "/public/blue_left.svg";
 import profile from "/public/profile.svg";
@@ -8,7 +9,17 @@ import db from "/public/server.svg";
 import plant from "/public/plant.svg";
 import submit from "/public/submit.svg";
 import upload from "/public/upload.svg";
+import check from "/public/tick.svg";
 const Header = () => {
+  const [text, settext] = useState(null);
+  const [apiData, setApiData] = useState("");
+
+  const api = () => {
+    chatreply(async (res) => {
+      data = res.success;
+      settext(data);
+    });
+  };
   return (
     <div
       style={{ backgroundColor: "#161618", width: "97%", margin: "0 auto" }}
@@ -26,8 +37,7 @@ const Header = () => {
           AI{" "}
           <span
             style={{
-              background:
-                "linear-gradient(90deg,#ff27c3 14.12%,#2496ff 71.08%);-webkit-background-clip: text;-webkit-text-fill-color: transparent;",
+                background: "linear-gradient(90deg,#ff27c3 14.12%,#2496ff 71.08%);-webkit-background-clip: text;-webkit-text-fill-color: transparent",
             }}
           >
             Structural Engineer
@@ -80,6 +90,8 @@ const Header = () => {
             placeholder="Start typing..."
             type="text"
             className="bg-transparent w-[550px] p-5 border-0 focus:outline-none "
+            value={apiData}
+            onChange={(e) => setApiData(e.target.value)}
           />
           <button type="submit">
             <Image src={submit} alt="submit" />{" "}
@@ -91,10 +103,83 @@ const Header = () => {
       </div>
 
       {/*right side */}
-      <div>
-        <Image src={upload} alt="upload" height={20} width={20} />
-        <p>Upload Documents to process</p>
-        
+      <div className="flex flex-col justify-centers items-end relative top-[-850px]">
+      <div >
+      <Image src={upload} alt="upload" height={20} width={20}/>
+        <p className="text-sm mt-5">Upload Documents to process</p>
+        <div className="flex gap-y-2 flex-col">
+          <div className="bg-[#212124] flex h-[60px] max-w-64 items-center p-3 rounded-lg">
+            <p className="mr-20 text-sm w-32">ASCE 7-16.pdf</p>
+            <Image src={check} alt="upload" height={30} width={30} />
+          </div>
+          <div className="bg-[#212124] flex h-[60px] max-w-64 items-center p-3 rounded-lg">
+            <p className="mr-20 text-sm w-32">Tolcco_report.pdf</p>
+            <Image src={check} alt="upload" height={30} width={30} />
+          </div>
+          <div className="bg-[#212124] flex h-[60px] max-w-64 items-center p-3 rounded-lg">
+            <p className="mr-20 text-sm w-32">sluicegate.pdf</p>
+            <Image src={check} alt="upload" height={30} width={30} />
+          </div>
+        </div>
+        </div>
+
+
+        <div className="mt-10">
+        <p className="text-sm">Use Preloaded Knowledge Base</p>
+        <div className="flex gap-y-2 flex-col">
+          <div className="bg-[#212124] flex h-[60px] max-w-64 items-center p-3 rounded-lg">
+            <p className="mr-20 text-sm w-32">ASCE 7-16.pdf</p>
+            <Image src={check} alt="upload" height={30} width={30} />
+          </div>
+          <div className="bg-[#212124] flex h-[60px] max-w-64 items-center p-3 rounded-lg">
+            <p className="mr-20 text-sm w-32">Tolcco_report.pdf</p>
+            <Image src={check} alt="upload" height={30} width={30} />
+          </div>
+          <div className="bg-[#212124] flex h-[60px] max-w-64 items-center p-3 rounded-lg">
+            <p className="mr-20 text-sm w-32">sluicegate.pdf</p>
+            <Image src={check} alt="upload" height={30} width={30} />
+          </div>
+          <div className="bg-[#212124] flex h-[60px] max-w-64 items-center p-3 rounded-lg">
+            <p className="mr-20 text-sm w-32">sluicegate.pdf</p>
+            <Image src={check} alt="upload" height={30} width={30} />
+          </div>
+        </div>
+        </div>
+
+
+        <div className="mt-10">
+        <p className="text-sm">Use Preloaded Knowledge Base</p>
+        <div className="flex gap-y-2 flex-col">
+          <div className="bg-[#212124] flex h-[60px] max-w-64 items-center p-3 rounded-lg">
+            <p className="mr-20 text-sm w-32">ASCE 7-16.pdf</p>
+            <Image src={check} alt="upload" height={30} width={30} />
+          </div>
+          <div className="bg-[#212124] flex h-[60px] max-w-64 items-center p-3 rounded-lg">
+            <p className="mr-20 text-sm w-32">Tolcco_report.pdf</p>
+            <Image src={check} alt="upload" height={30} width={30} />
+          </div>
+          <div className="bg-[#212124] flex h-[60px] max-w-64 items-center p-3 rounded-lg">
+            <p className="mr-20 text-sm w-32">sluicegate.pdf</p>
+            <Image src={check} alt="upload" height={30} width={30} />
+          </div>
+          <div className="bg-[#212124] flex h-[60px] max-w-64 items-center p-3 rounded-lg">
+            <p className="mr-20 text-sm w-32">sluicegate.pdf</p>
+            <Image src={check} alt="upload" height={30} width={30} />
+          </div>
+          <div className="bg-[#212124] flex h-[60px] max-w-64 items-center p-3 rounded-lg">
+            <p className="mr-20 text-sm w-32">sluicegate.pdf</p>
+            <Image src={check} alt="upload" height={30} width={30} />
+          </div>
+          <div className="bg-[#212124] flex h-[60px] max-w-64 items-center p-3 rounded-lg">
+            <p className="mr-20 text-sm w-32">sluicegate.pdf</p>
+            <Image src={check} alt="upload" height={30} width={30} />
+          </div>
+          <div className="bg-[#212124] flex h-[60px] max-w-64 items-center p-3 rounded-lg">
+            <p className="mr-20 text-sm w-32">sluicegate.pdf</p>
+            <Image src={check} alt="upload" height={30} width={30} />
+          </div>
+        </div>
+        </div>
       </div>
     </div>
   );
---
+++ src/app/api/Chatmain.jsx
@@ -0,0 +1,31 @@
+const axios = require('axios');
+const fs = require('fs');
+const FormData = require('form-data');
+
+const BASE_URL = 'https://ashva.pythonanywhere.com/upload';
+const filepath = "/Users/bhoshaga/Downloads/ret.jpeg";
+
+const data = {
+    user_message: 'can you build a 3D model?',
+};
+
+const formData = new FormData();
+formData.append('file', fs.createReadStream(filepath));
+
+axios.post(BASE_URL, formData, {
+    headers: {
+        ...formData.getHeaders(),
+    },
+    data: data,
+})
+    .then(response => {
+        const resp = response.data;
+        console.log(resp);   // Full response by server, dictionary with keys: status, message, status_code, server_response, filename
+        
+        console.log(resp.message);  // this is the AI chatbot response
+        console.log(resp.server_response); // three js model provided by server, this is what we want
+  })
+    
+    .catch(error => {
+        console.error(error);
+    });
